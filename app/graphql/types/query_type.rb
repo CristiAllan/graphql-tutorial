763a25123ae089b01972cf9632a5e7e426c38033
Cristi Allan <cristiallan@gmail.com>
2021-01-28 13:40:31 -0300

    add User model and setup type and mutation to credentials

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,9 +1,14 @@
 module Types
   class QueryType < Types::BaseObject
     field :all_links, [LinkType], null: false
+    field :all_users, [UserType], null: false
 
     def all_links
       Link.all
+    end
+
+    def all_users
+      User.all
     end
   end
 end
